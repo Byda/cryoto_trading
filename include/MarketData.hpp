96a4321444c516012608e447b5b6f9302e0db0e2
Đoàn Duy <doanduy@Duys-MacBook-Pro.local>
2025-02-24 21:35:41 +0700

    First commit

---
+++ include/MarketData.hpp
@@ -0,0 +1,16 @@
+#ifndef MARKET_DATA_HPP
+#define MARKET_DATA_HPP
+
+#include <string>
+
+struct PriceData
+{
+    std::string symbol;
+    double price;
+};
+
+class MarketData{
+public:
+    MarketData();
+    PriceData getPrice(const std::string& symbol);
+};
